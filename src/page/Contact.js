f336e32d9b74b0e307a2f2b23865009b91ad68e1
mishachornyy <85844519+mishachornyy@users.noreply.github.com>
2022-09-14 21:06:22 -0600

    Add files via upload

---
+++ src/page/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../style/Contact.css"
+import Footer from "../component/Footer";
+
+function Contact() {
+  return ( <>
+
+      <div className="contact">
+
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default Contact;
